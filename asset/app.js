f80825067297e57098ef30cb7a4681bc5f6f030d
manucarenjot <carenjotje@gmail.com>
2022-11-07 16:32:19 +0100

    fini

---
+++ asset/app.js
@@ -0,0 +1,11 @@
+let array = ['fizzbuzz', 'nothing', 'nothing', 'fizz', 'nothing', 'buzz',];
+
+function transform(key) {
+
+    for (i=0; i<key.length; i++) {
+        console.log(array[i])
+    }
+
+}
+
+transform([1,3,4]);
